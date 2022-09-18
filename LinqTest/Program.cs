90e27a87adad925dca332fa4aee80dbcd5773f6e
osajab10@gmail.com <osajab10@gmail.com>
2022-09-18 15:40:18 +0200

    LINQ querie

--- LinqTest/Program.cs
+++ LinqTest/Program.cs
@@ -19,6 +19,16 @@ namespace LinqTest
             {
                 Console.WriteLine(i);  
             }
+
+            int[] values = new int[] { 0, 12, 44, 36, 92, 54, 13, 8 };
+            IEnumerable<int> result =
+                from v in values
+                where v < 37
+                orderby -v
+                select v;
+
+            foreach (int i in result)
+                Console.Write($"{i} ");
         }
 
     }
